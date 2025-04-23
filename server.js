7b2009a72b84e2d112684ccceed5253bbedfa209
kyoung2-lee <kyoung60003@gmail.com>
2025-04-23 18:34:26 +0900

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 const PORT = 3000;
-app.arguments(express.static("dist"));
+app.use(express.static("dist"));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
